e6b17047aaccf41c62984e841d47cec88f4ddf66
Ippei Nagamine <1pei.nag@gmail.com>
2026-01-04 08:53:13 +0900

    Complete project initialization with Capacitor config and iOS platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.retrogamebox.app',
+  appName: 'Retro Game Box',
+  webDir: 'dist'
+};
+
+export default config;
